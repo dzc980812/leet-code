26010e58eb0a44d6321eac7ca6627a77e377257a
zc <980861111@qq.com>
2022-04-13 17:32:15 +0800

    leetcode 58. 最后一个单词的长度

---
+++ main/58. Length of Last Word.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  let res = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    console.log(s[i], 's[i]')
+    console.log(res, 'res')
+    if (s[i] == ' ' && res) {
+      break;
+    } else if (s[i] !== ' ') {
+      res++
+    }
+  }
+
+  return res
+};
+
+console.log(lengthOfLastWord('a cdab'))
