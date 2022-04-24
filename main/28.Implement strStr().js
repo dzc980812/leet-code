4198e5e0b6190cb0eb0438ae4bf1c1af0bb71d65
zc <980861111@qq.com>
2022-04-24 16:08:01 +0800

    补充题目描述

--- main/28.Implement strStr().js
+++ main/28.Implement strStr().js
@@ -2,6 +2,11 @@
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
+ * 实现 strStr() 函数。
+   给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回  -1 。
+   来源：力扣（LeetCode）
+   链接：https://leetcode-cn.com/problems/implement-strstr
+   著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 var strStr = function (haystack, needle) {
   if (needle.length === 0) return 0;
--- main/917. Reverse Only Letters.js
+++ main/917. Reverse Only Letters.js
@@ -2,12 +2,11 @@
  * @param {string} s
  * @return {string}
  * 给你一个字符串 s ，根据下述规则反转字符串：
-
-    所有非英文字母保留在原有位置。
-    所有英文字母（小写或大写）位置反转。
-    返回反转后的 s 。
-    输入：s = "ab-cd"
-    输出："dc-ba"
+   所有非英文字母保留在原有位置。
+   所有英文字母（小写或大写）位置反转。
+   返回反转后的 s 。
+   输入：s = "ab-cd"
+   输出："dc-ba"
  */
 var reverseOnlyLetters = function (s) {
   let l = 0,
